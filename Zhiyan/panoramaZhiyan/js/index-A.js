82cd7f701b0dd310e89425f803ddef9ca385dd0f
ChinaLocalHistory <chinalocalhistory@gmail.com>
2022-06-08 16:11:56 -0400

    fixing some error

--- Zhiyan/panoramaZhiyan/js/index-A.js
+++ Zhiyan/panoramaZhiyan/js/index-A.js
@@ -74,7 +74,7 @@ var v=pannellum.viewer('panorama', {
                 {
                     "pitch":-0.28860810255934893,
                     "yaw":77.74459708662397,
-                    "type": "scenes",
+                    "type": "scene",
                     "text": "B.1 The Hall of Continuing Virtue, Yande Tang exterior",
                     "sceneID":"B1",
                     "clickHandlerFunc": toB1,
