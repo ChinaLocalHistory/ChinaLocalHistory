c5908eeb07e3cbe8da3dfe182ff3c6dfd8be47d4
ChinaLocalHistory <chinalocalhistory@gmail.com>
2022-08-02 15:45:32 -0400

    fixing the js of Lu

--- Lu/panoramaLu/js/index-Q.js
+++ Lu/panoramaLu/js/index-Q.js
@@ -9,7 +9,7 @@ ReactDOM.render(gallery, document.getElementById('react-gallery'));
 //generating the panorama
 var v=pannellum.viewer('panorama', {
     "default":{
-        "firstScene":"B",
+        "firstScene":"Q1",
         "sceneFadeDuration": 1000
     },
 
